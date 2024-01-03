8583780b906d99a3fff08a9eb3c33fb729695cd4
Rainy_energe <143769927+JVing-KW@users.noreply.github.com>
2024-01-03 23:13:21 +0900

    [level unrated] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 2;
+      int a  =  (int)Math.sqrt(n);
+        if(a*a == n){
+            answer = 1;
+        }
+        return answer;
+    }
+}
