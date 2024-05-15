2ef1b7bcf8dbcced2b829495757404ecc4c9c5bb
Rainy_energe <143769927+JVing-KW@users.noreply.github.com>
2024-05-16 03:44:43 +0900

    [Silver II] Title: 예산, Time: 160 ms, Memory: 15744 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -1,60 +1,77 @@
 
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.StringTokenizer;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        String[] str = br.readLine().split(" ");
-        int[] arr = new int[N];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Integer.parseInt(str[i]);
-        }
-        int M = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws IOException {
 
-        bw.write(String.valueOf(upper(arr, M)));
-        bw.flush();
-        bw.close();
-    }
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		String st = br.readLine();
+		String[] str = st.split(" ");
+		int[] arr = new int[N];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(str[i]);
+		}
+		//예산 
+		int M = Integer.parseInt(br.readLine());
+//		5
+//		70 80 30 40 100
+//		450
 
-    public static long upper(int[] arr, int max) {
-        int hi = 0;
-        int lo = 0;
-        long sum = 0;
+		System.out.print(Main.upper(arr,M));
+	}
 
-        for (int value : arr) {
-            sum += value;
-            if (value > hi) {
-                hi = value;
-            }
-        }
+	public static long upper(int[] arr, int max) {
+		int hi = 0;
+		int lo = 0;
+		long answer=0;
+		long sum = 0;
+		// mid 값이 필요 lo 는 0 그대로 쓰고 .
+		for (int i = 0; i < arr.length; i++) {
 
-        if (sum <= max) return hi;
+			sum +=arr[i];
+			if (arr[i] > hi) {
+				hi = arr[i];
+			}
+			
+		}
+		if(sum < max)return hi;
+		
 
-        long answer = 0;
-        while (hi >= lo) {
-            sum = 0;
-            int mid = (hi + lo) / 2;
+		while (hi >= lo) { 
+			// 전체 비용 초기화.
+			sum = 0;
+			
+			
+			int mid = (hi + lo) / 2;
+			for (int i = 0; i < arr.length; i++) {
+				// 	배열의 값을 변경하지 않고 mid 값만 이용.
+				if(arr[i]>mid) sum+=mid;
+				
+                else sum += arr[i];
 
-            for (int value : arr) {
-                sum += Math.min(value, mid);
-            }
+			}
+			if(sum > max) {
+			hi = mid-1;
+				
+			}else {
+			lo = mid+1;
+			answer = mid;
 
-            if (sum > max) {
-                hi = mid - 1;
-            } else {
-                lo = mid + 1;
-                answer = mid;
-            }
-        }
+			}
 
-        return answer;
-    }
+		}
+		return answer;
+	}
 }
